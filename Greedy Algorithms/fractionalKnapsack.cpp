b9346b9889a659829707eb506f38e7096a7c8d6b
adarshgupta404 <gadarsh747@gmail.com>
2023-04-02 12:45:45 +0530

    first

--- Greedy Algorithms/fractionalKnapsack.cpp
+++ Greedy Algorithms/fractionalKnapsack.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
     int capacity, no_items, cur_weight, item;
-    int used[10];
+    float used[10];
     float total_profit;
     int i;
     int weight[10];
     int value[10];
-
+    int X[10];
     printf("Enter the capacity of knapsack:\n");
     scanf("%d", &capacity);
 
@@ -39,16 +39,25 @@ void main()
         used[item] = 1;
         cur_weight -= weight[item];
         total_profit += value[item];
-        if (cur_weight >= 0)
+        if (cur_weight >= 0){
             printf("Added object %d (%d Rs., %dKg) completely in the bag. Space left: %d.\n", item + 1, value[item], weight[item], cur_weight);
+        }
         else
         {
             int item_percent = (int) ((1 + (float) cur_weight / weight[item]) * 100);
             printf("Added %d%% (%d Rs., %dKg) of object %d in the bag.\n", item_percent, value[item], weight[item], item + 1);
             total_profit -= value[item];
+            used[item] = 1 + (float) cur_weight / weight[item];
             total_profit += (1 + (float)cur_weight / weight[item]) * value[item];
         }
     }
 
-    printf("Filled the bag with objects worth %.2f Rs.\n", total_profit);
+    printf("Filled the bag with objects worth Rs. %.2f\nResultant Vector: ", total_profit);
+    for (int i = 0; i < no_items; i++)
+    {
+        printf("|%.2f| ", used[i]);
+    }
+    
+    return 0;
+
 }
